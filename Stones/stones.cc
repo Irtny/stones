627f70727c4377bd670da6efee87568e2ab6ba9e
Домашний <@192.168.56.1>
2020-11-17 19:22:21 +0500

    Добавлена статистика, улучшен ИИ, раздел сложности

--- Stones/stones.cc
+++ Stones/stones.cc
@@ -1,9 +1,3 @@
-/*
- * stones.cc
- *
- *  Created on: 11 нояб. 2020 г.
- *      Author: Домашний
- */
 
 
 #include <cstdlib>
@@ -16,7 +10,10 @@
 #include <chrono>
 
 
-//Ввод данных и проверки
+int WAI=0;
+int WU=0;
+
+//Input and check
 int get_int(const char *question) {
 	int result;
 	std::string inp;
@@ -28,13 +25,13 @@ int get_int(const char *question) {
 		{
 			if (inps.fail()) {
 				if (std::cin.eof()) {
-					std::cout << "Ошибка ввода/вывода" << std::endl;
+					std::cout << "Input/Output error" << std::endl;
 					std::exit(1);
 				}
-				std::cout << "Некорректный ввод" << std::endl;
+				std::cout << "Incorrect input" << std::endl;
 				goto re;
 			} else if (not inps.eof()) {
-				std::cout << "Некорректный ввод" << std::endl;
+				std::cout << "Incorrect input" << std::endl;
 				goto re;
 			}
 		}
@@ -44,7 +41,7 @@ int get_int(const char *question) {
 	return result;
 }
 
-int read_int_in_range(const char *question, int min, int max) {
+int getAndCheckInRange(const char *question, int min, int max) {
 	for (;;) {
 		int returnInt = get_int(question);
 		if (returnInt >= min && returnInt <= max) {
@@ -55,9 +52,23 @@ int read_int_in_range(const char *question, int min, int max) {
 	}
 	return 0;
 }
+
+int read_int_in_range(const char *question, int min, int max) {
+	for (;;) {
+		int returnInt = get_int(question);
+		if (returnInt >= min && returnInt <= max) {
+			return returnInt;
+		}
+		std::cout << "Input correct number!" << std::endl;
+		continue;
+	}
+	return 0;
+}
+
+
 int main()
 {
-	//Определяем случайное количестов начальных камней
+
 	qre1:using clk=std::chrono::system_clock;
 	std::minstd_rand rnd { unsigned(clk::now().time_since_epoch().count()) };
 
@@ -65,108 +76,139 @@ int main()
 	std::uniform_int_distribution <int> min_stone{1,3};
 	unsigned stone_count = d(rnd);
 	unsigned stone;
-	int hard;
+	int hard=1;
 
+	for (;;) {
+
+				std::cout << "МЕНЮ \n"
+						"1 | Play \n"
+						"2 | Difficulty \n"
+						"3 | Statistic \n"
+						"4 | Help \n"
+						"5 | Exit" << std::endl;
+				int intForSwitch = getAndCheckInRange("Select menu item", 1, 5);
+				switch (intForSwitch) {
+				case 1:
+					goto qre2;
+					break;
+				case 2:
+					std::cout << "Difficulty \n"
+						"1 | Easy \n"
+						"2 | Hard \n"
+						 << std::endl;
+					hard = getAndCheckInRange("Select difficulty", 1, 2);
+					break;
+				case 3:
+					std::cout << "Statistic of wins \n"
+								"USER | " << WU <<" \n"
+								"AI | " << WAI <<" \n"
+									<< std::endl;
+					break;
+				case 4:
+					std::cout << "It is easy(no) game \n"
+											"take 1-3 stones \n"
+											"AI turn \n"
+											"Repeat \n"
+											"if you take last stone - AI WIN \n"
+											"else - YOU" << std::endl;
+					break;
+				case 5:
+					exit(0);
+					break;
+				}
+			}
+			return 0;
 
 
 	do{
 
+		qre2:if (stone_count==0){
+				std::cout << "USER WIN!" << std::endl;
+				::WU++;
+				break;
+				}
+		if(stone_count==1){
+					std::cout << "AI WIN!" << std::endl;
+					::WAI++;
+					break;
+				}
 
-
-		std::cout << "В куче " << stone_count << " камней." << std::endl;
-		stone =read_int_in_range("Сколько камней взять? (от 1 до 3) ",1,std::min(3U,stone_count));
+		std::cout << "In heap " << stone_count << " stones." << std::endl;
+		stone =read_int_in_range("How much take? (1-3) ",1,std::min(3U,stone_count));
 		stone_count-=stone;
-		//<algorithm>
+		//<algorithm
 
+		if(stone_count==stone){
+			std::cout << "AI WIN!" << std::endl;
+			::WAI++;
+			goto qre1;
 
-		{
-			if (stone_count==0){
-						std::cout << "ИИ Победил!" << std::endl;
-						break;
-					}
-					else if(stone_count==1){
-						std::cout << "ИИ Проиграл!" << std::endl;
-						break;
-					}
+		}
 
-			qwe1:stone=min_stone(rnd);
+			qwe2:
+			stone=min_stone(rnd);
 
-			//Лёгкий ИИ
+			//Easy AI
 
-			if (hard==0){
-				if(stone<=3 && stone<=stone_count && stone>=1){
+			if (hard==1){
+				if(stone<=3 && stone<=stone_count){
 									stone_count-=stone;
 							}
-				else{// std::cout <<  << std::endl;
-									goto qwe1;
-
-							}
+				else{
+					goto qwe2;
+						}
+				std::cout << "Bot take " << stone << " stones." << std::endl;
+				goto qre2;
 			}
 
-			//Cложный ИИ
-			if (hard==1){
-				if(stone<=3 && stone<=stone_count && stone>=1 && stone_count>8){
-						stone_count-=stone;
+			//Hard AI
+			if (hard==2){
+				if(stone_count==3){
+					stone=2;
+					stone_count-=stone;
 			}
-			else if(stone_count<=8){
-				switch ( stone_count ) {
-				case 8:
+				if(stone_count==2){
+					stone=1;
+					stone_count-=stone;
+			}
+
+			else if(stone_count>=4){
+				switch ( stone_count%4 ) {
+				case 0:
 					stone=3;
 					stone_count-=stone;
 					break;
-				case 7:
+				case 1:
 					stone=2;
 					stone_count-=stone;
 					break;
-				case 6:
-					stone=1;
-					stone_count-=stone;
-					break;
-				case 5:
-					stone=1;
-					stone_count-=stone;
-					break;
-				case 4:
-				  stone=3;
-				  stone_count-=stone;
-				  break;
-				case 3:
-					stone=2;
-					stone_count-=stone;
-				  break;
 				case 2:
 					stone=1;
 					stone_count-=stone;
+					break;
+				case 3:
+					stone=2;
+					stone_count-=stone;
+					break;
+
 
 
 				  break;
 				}
-			}
-			}
-
-			else{// std::cout <<  << std::endl;
-					goto qwe1;
 
 			}
+			else if(stone_count==1){
+				stone_count--;
+				}
+				std::cout << "Bot take " << stone << " stones." << std::endl;
+				goto qre2;
 
-			//std::cout << stone_count << std::endl;
-			std::cout << "Бот взял " << stone << " камней." << std::endl;
-		}
-
-	} while(stone_count!=0);
-	int h;
-	std::cout <<"Начать сначала? (1 - Да, 2 - Нет)" <<std::endl;
-	std::cin >>h;
-	switch(h){
-	case 1:
-		goto qre1;
-		break;
-	case 2:
-		exit(0);
 
 	}
+	} while(stone_count!=0);
 
-//	}
-	return 0;
-}
+
+		goto qre1;
+
+	}
 
